1124def143f4eba67d604d6c3e6fabd3365cb1e1
Eduardo dos Santos Barbieri <eduardo.santos@compasso.com.br>
2017-11-28 17:27:49 -0200

    Component NavBar

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,18 @@
 <template>
+  
   <div id="app">
-    <div class="navbar">
-        <h3>Dynamo Req {{$store.global.environment}}</h3>
-    </div>
+    <navbar></navbar>
 
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import Navbar from  '@/components/Navbar';
   export default {
+    components:{
+      Navbar
+    },
     name: 'dynareq-ui',
     store: {
         global: 'global'
--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <router-link to="/registerip">Go to</router-link>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <p class="title">
+          Documentation
+        </p>
+        <p class="subtitle">
+          Everything you need to <strong>create a website</strong> with Bulma
+        </p>
+      </div>
+    </section>
   </div>
 </template>
 
