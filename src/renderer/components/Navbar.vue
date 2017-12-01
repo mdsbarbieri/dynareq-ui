2f68f375aa24c25669b83dad1200fbe75392d748
Matheus Barbieri <mdsbarbieri@gmail.com>
2017-12-01 17:24:07 -0200

    Add logo an fix navbar in mac

--- src/renderer/components/Navbar.vue
+++ src/renderer/components/Navbar.vue
@@ -1,15 +1,22 @@
 <template>
-    <nav class="navbar is-dark is-fixed-top">
-        <div class="navbar-brand">
+    <nav class="navbar is-dark is-fixed-top" v-bind:class="{ isMac: isMac}">
+        <div class="navbar-brand" >
             <a class="navbar-item">
-                <h1>DynareqUI</h1>
+                <img src="../assets/image/logo.png"/>
             </a>
         </div>
     </nav>
 </template>
 <script>
+    import os from 'os';
+    import _ from "lodash";
     export default {
         name: 'navbar',
-        store: ['request']
+        store: ['request'],
+        data(){
+            return {
+                isMac : (_.isEqual(os.platform(), 'darwin')) ? true : false
+            }
+        }
     }
 </script>
